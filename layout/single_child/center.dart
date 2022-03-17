3328f4afebd14fb4275d690b4ee85adea7ca6212
作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-03-17 09:23:22 +0900

    Create center.dart

---
+++ layout/single_child/center.dart
@@ -0,0 +1,61 @@
+/// How to display widgets in the middle.
+/// （ウィジェットを真ん中に表示する方法）
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+        primarySwatch: Colors.yellow,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Center'),
+      ),
+
+      // Center allows widgets to be displayed in the middle.
+      // （Center でウィジェットを真ん中に表示できる）
+      // It's okay to put it after return.
+      // （return の次に置いても平気）
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'Display in the middle.',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 38,
+              ),
+            ),
+            Text(
+              '真ん中に表示',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 65,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
