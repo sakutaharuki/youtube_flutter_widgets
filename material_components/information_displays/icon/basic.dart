ed070908429de0dd8a50e95b68e40bb32ba7f709
作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-03-19 20:40:14 +0900

    Create basic.dart

---
+++ material_components/information_displays/icon/basic.dart
@@ -0,0 +1,46 @@
+// Youtube URL:  
+// This is the code presented in the URL above.  
+// （上記URLで紹介されているコードです。）
+
+import 'package:flutter/material.dart';
+
+/// How to display Icon.
+/// （Icon を表示する方法）
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black,
+        primarySwatch: Colors.yellow,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Icon'),
+      ),
+      body: const Center(
+        child: Icon(
+          Icons.audiotrack_rounded,
+          color: Colors.pink,
+          size: 380.0,
+        ),
+      ),
+    );
+  }
+}
