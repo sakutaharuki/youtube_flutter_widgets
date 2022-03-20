715e66bb6fd17f62a59d4cfcda2898f3b6f33036
作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-03-20 11:38:42 +0900

    Update basic.dart

--- text/rich_text/basic.dart
+++ text/rich_text/basic.dart
@@ -1,3 +1,7 @@
+// Youtube URL:  
+// This is the code presented in the URL above.  
+// （上記URLで紹介されているコードです。）
+
 import 'package:flutter/material.dart';
 
 /// How to combine bold and color in text.
