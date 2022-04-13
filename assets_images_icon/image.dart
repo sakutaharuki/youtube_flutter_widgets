b537ba2f90f80663f8e34b9c5399428b6972f89f
作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-04-13 11:07:21 +0900

    Create image.dart

---
+++ assets_images_icon/image.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+/// How to display prepared images.
+/// 用意した画像を表示する方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Asset'),
+      ),
+
+      /// Show Image
+      /// 画像を表示
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/image1.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
