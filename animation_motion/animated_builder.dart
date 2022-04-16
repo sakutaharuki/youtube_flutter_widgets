8143633fb3b827ebeb12ed4bc1a6e8dd6f06cfbf
作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-04-16 21:56:21 +0900

    Update animated_builder.dart

--- animation_motion/animated_builder.dart
+++ animation_motion/animated_builder.dart
@@ -1,3 +1,7 @@
+// Youtube URL: https://youtu.be/TIiUGY3p0H8
+// This is the code presented in the URL above.  
+// （上記URLで紹介されているコードです。）
+
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
