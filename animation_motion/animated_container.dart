ef85aa7b61df78e6655ae259be6a12cc58a6b5d2
作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-04-17 15:21:23 +0900

    Update animated_container.dart

--- animation_motion/animated_container.dart
+++ animation_motion/animated_container.dart
@@ -1,3 +1,7 @@
+// Youtube URL: 
+// This is the code presented in the URL above.  
+// （上記URLで紹介されているコードです。）
+
 import 'package:flutter/material.dart';
 
 /// How to change automatically over a period of time.
