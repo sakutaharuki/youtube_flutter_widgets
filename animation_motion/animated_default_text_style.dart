d52d3b7bc13b4f29e103662ced5dcf58f811d08f
作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-04-19 19:27:50 +0900

    Create animated_default_text_style.dart

---
+++ animation_motion/animated_default_text_style.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+/// How to change the text layout automatically.
+/// 文字のレイアウトを自動的に変更する方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text("AnimatedCrossFade")),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    /// Tap Settings
+    /// タップ時の設定
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: ListView(
+        children: [
+          Center(
+            child: AnimatedDefaultTextStyle(
+              child: Container(
+                margin: const EdgeInsets.all(20.0),
+                padding: const EdgeInsets.all(20.0),
+                child: const Text('Flutter'),
+              ),
+              style: selected
+                  ? const TextStyle(
+                      color: Colors.blue,
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 4,
+                    )
+                  : const TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.w100,
+                      fontSize: 25,
+                      letterSpacing: 2,
+                    ),
+              duration: const Duration(seconds: 10),
+              curve: Curves.elasticOut,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Ink(
+            decoration: const ShapeDecoration(
+              color: Colors.purple,
+              shape: CircleBorder(),
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.heart_broken),
+              iconSize: 40,
+              color: Colors.white,
+              onPressed: () {
+                setState(() {
+                  selected = !selected;
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
