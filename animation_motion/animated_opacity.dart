b6bfd01ce9d202a4f23aad5e2d8af81b0ed7e970
作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-04-22 23:13:27 +0900

    Update animated_opacity.dart

--- animation_motion/animated_opacity.dart
+++ animation_motion/animated_opacity.dart
@@ -1,1 +1,69 @@
+// Youtube URL: https://youtu.be/QRtMFX3sf1Y
+// This is the code presented in the URL above.  
+// （上記URLで紹介されているコードです。）
 
+import 'package:flutter/material.dart';
+
+/// How to make widgets transparent.
+/// ウィジェットを透明にする方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('AnimatedOpacity')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  double opacityLevel = 1.0;
+
+  void _changeOpacity() {
+    setState(() => opacityLevel = opacityLevel == 0 ? 1.0 : 0.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          AnimatedOpacity(
+            opacity: opacityLevel,
+            duration: const Duration(seconds: 1),
+            child: const FlutterLogo(
+              size: 400,
+            ),
+          ),
+          ElevatedButton(
+            child: const Text(
+              '透明化！',
+              style: TextStyle(fontSize: 50.0),
+            ),
+            onPressed: _changeOpacity,
+          ),
+        ],
+      ),
+    );
+  }
+}
