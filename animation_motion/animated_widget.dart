e8873cbb28ba7654d5c2b510dbcb608ac5b6a9d5
Haruki Sakuta_作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-05-04 16:09:19 +0900

    Create animated_widget.dart

---
+++ animation_motion/animated_widget.dart
@@ -0,0 +1,77 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+/// Basic method of animating widgets.
+/// ウィジェットにアニメーションをつける基本の方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('AnimatedWidget')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+/// Adding animations
+/// アニメーションを追加する
+class SpinningContainer extends AnimatedWidget {
+  const SpinningContainer({
+    Key? key,
+    required AnimationController controller,
+  }) : super(key: key, listenable: controller);
+
+  Animation<double> get _progress => listenable as Animation<double>;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: _progress.value * 2.0 * math.pi,
+      child: Container(width: 200.0, height: 200.0, color: Colors.orange),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 10),
+    vsync: this,
+  )..repeat();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        /// Display of created animations
+        /// 作ったアニメーションの表示
+        child: SpinningContainer(controller: _controller),
+      ),
+    );
+  }
+}
