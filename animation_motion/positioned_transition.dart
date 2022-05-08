16cbc4a480731a82a3996fe61d78774f3a39a359
Haruki Sakuta_作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-05-08 11:52:32 +0900

    Update positioned_transition.dart

--- animation_motion/positioned_transition.dart
+++ animation_motion/positioned_transition.dart
@@ -1,3 +1,80 @@
 // Youtube URL: 
 // This is the code presented in the URL above.  
 // （上記URLで紹介されているコードです。）
+
+import 'package:flutter/material.dart';
+
+/// .
+///
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('PositionedTransition')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const double smallLogo = 100;
+    const double bigLogo = 200;
+
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final Size biggest = constraints.biggest;
+        return Stack(
+          children: <Widget>[
+            PositionedTransition(
+              rect: RelativeRectTween(
+                begin: RelativeRect.fromSize(
+                    const Rect.fromLTWH(0, 0, smallLogo, smallLogo), biggest),
+                end: RelativeRect.fromSize(
+                    Rect.fromLTWH(biggest.width - bigLogo,
+                        biggest.height - bigLogo, bigLogo, bigLogo),
+                    biggest),
+              ).animate(CurvedAnimation(
+                parent: _controller,
+                curve: Curves.elasticInOut,
+              )),
+              child: const Padding(
+                  padding: EdgeInsets.all(8), child: FlutterLogo()),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
