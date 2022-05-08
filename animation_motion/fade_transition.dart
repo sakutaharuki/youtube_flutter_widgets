06a9b6c2503ffb2ea8aded83d9f6a602f138e4d1
Haruki Sakuta_作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-05-08 11:55:17 +0900

    Update fade_transition.dart

--- animation_motion/fade_transition.dart
+++ animation_motion/fade_transition.dart
@@ -1,3 +1,75 @@
 // Youtube URL: 
 // This is the code presented in the URL above.  
 // （上記URLで紹介されているコードです。）
+
+import 'package:flutter/material.dart';
+
+/// How to make a widget transparent in an animation.
+/// アニメーションでウィジェットを透明にする方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('FadeTransition')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> with TickerProviderStateMixin {
+  /// Transparency content setting.
+  /// 透明度の内容設定
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+
+      /// Transparency setting.
+      /// 透明度の設定
+      child: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: const Padding(
+              padding: EdgeInsets.all(8),
+              child: FlutterLogo(
+                size: 400,
+              )),
+        ),
+      ),
+    );
+  }
+}
