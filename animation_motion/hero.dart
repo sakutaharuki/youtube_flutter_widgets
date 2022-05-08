7a734ef458f8e3ed0dfde300659ec6550904e853
Haruki Sakuta_作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-05-08 11:53:02 +0900

    Update hero.dart

--- animation_motion/hero.dart
+++ animation_motion/hero.dart
@@ -1,3 +1,88 @@
 // Youtube URL: 
 // This is the code presented in the URL above.  
 // （上記URLで紹介されているコードです。）
+
+import 'package:flutter/material.dart';
+
+/// .
+///
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Hero')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const SizedBox(
+          height: 20.0,
+        ),
+        ListTile(
+          leading: Hero(
+            tag: 'hero-rectangle',
+            child: _blueRectangle(const Size(50, 50)),
+          ),
+          onTap: () => _gotoDetailsPage(context),
+          title:
+              const Text('Tap on the icon to view hero animation transition.'),
+        ),
+      ],
+    );
+  }
+
+  Widget _blueRectangle(Size size) {
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: Colors.blue,
+    );
+  }
+
+  void _gotoDetailsPage(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('second Page'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Hero(
+                tag: 'hero-rectangle',
+                child: _blueRectangle(const Size(200, 200)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
