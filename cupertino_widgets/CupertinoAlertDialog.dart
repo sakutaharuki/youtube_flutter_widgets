a6354495bdea666b0682414abca9abf0c245a557
Haruki Sakuta_作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-05-23 10:10:44 +0900

    Update CupertinoAlertDialog.dart

--- cupertino_widgets/CupertinoAlertDialog.dart
+++ cupertino_widgets/CupertinoAlertDialog.dart
@@ -1,3 +1,88 @@
 // Youtube URL: 
 // This is the code presented in the URL above.  
 // （上記URLで紹介されているコードです。）
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// How to display Apple's design alert dialog.
+/// Appleのデザインのアラートダイアログを表示する方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('CupertinoAlertDialog')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  void _showAlertDialog(BuildContext context) {
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: const Text('Alert'),
+        content: const Text('人生逆転させてみる？'),
+        actions: <CupertinoDialogAction>[
+          CupertinoDialogAction(
+            /// This action is the default
+            /// Text in bold
+            /// このアクションがデフォルト
+            /// テキストは太太字
+            isDefaultAction: true,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('No'),
+          ),
+          CupertinoDialogAction(
+            /// Deletion Action
+            /// Text color is red
+            /// 削除のアクション
+            /// テキスト色は赤
+            isDestructiveAction: true,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Yes'),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: CupertinoButton(
+          onPressed: () => _showAlertDialog(context),
+          child: const Text(
+            'CupertinoAlertDialog\nオープン！',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.deepOrange,
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
