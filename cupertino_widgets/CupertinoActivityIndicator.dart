3e2b1baa2961dd245ecd86b13b8b0dd3cc3a369e
Haruki Sakuta_作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-05-23 09:54:35 +0900

    Update CupertinoActivityIndicator.dart

--- cupertino_widgets/CupertinoActivityIndicator.dart
+++ cupertino_widgets/CupertinoActivityIndicator.dart
@@ -1,3 +1,46 @@
 // Youtube URL: 
 // This is the code presented in the URL above.  
 // （上記URLで紹介されているコードです。）
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// How to display indicators that can be used while data is being loaded.
+/// データの読み込み中に使えるインジケーターを表示する方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('CupertinoActivityIndicator')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      /// Indicator Settings
+      /// インジケータの設定
+      child: CupertinoActivityIndicator(
+        animating: true,
+        radius: 80,
+      ),
+    );
+  }
+}
