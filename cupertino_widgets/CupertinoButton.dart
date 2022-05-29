cbea3cd6f3970c110df07351dbf7310dd868353f
Haruki Sakuta_作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-05-29 12:07:54 +0900

    Update CupertinoButton.dart

--- cupertino_widgets/CupertinoButton.dart
+++ cupertino_widgets/CupertinoButton.dart
@@ -1,3 +1,87 @@
 // Youtube URL: 
 // This is the code presented in the URL above.  
 // （上記URLで紹介されているコードです。）
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// How to display Apple's design button.
+/// Appleのデザインのボタンを表示する方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('CupertinoButton')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const CupertinoButton(
+            onPressed: null,
+            child: Text(
+              '押せない状態（Disabled）',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(height: 40),
+          const CupertinoButton.filled(
+            onPressed: null,
+            child: Text(
+              '押せない状態（Disabled）',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(height: 40),
+          CupertinoButton(
+            onPressed: () {},
+            child: const Text(
+              '押せる状態（Enabled）',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(height: 40),
+          CupertinoButton.filled(
+            onPressed: () {},
+            child: const Text(
+              '押せる状態（Enabled）',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
