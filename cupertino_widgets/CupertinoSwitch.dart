c7ac9b3b05f12e912ace6ec7a4786eb762add0f1
Haruki Sakuta_作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-06-04 11:25:38 +0900

    Update CupertinoSwitch.dart

--- cupertino_widgets/CupertinoSwitch.dart
+++ cupertino_widgets/CupertinoSwitch.dart
@@ -1,3 +1,65 @@
 // Youtube URL: 
 // This is the code presented in the URL above.  
 // （上記URLで紹介されているコードです。）
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// How to use Apple's design switches.
+/// Appleのデザインのスイッチを使う方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('CupertinoSwitch')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  var _lights = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Center(
+        child: MergeSemantics(
+          child: ListTile(
+            title: const Text('（例）ダークモード切り替え'),
+
+            /// Switch settings
+            /// スイッチの設定
+            trailing: CupertinoSwitch(
+              value: _lights,
+              onChanged: (bool value) {
+                setState(() {
+                  _lights = value;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
