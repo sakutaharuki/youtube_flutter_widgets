81418848883fcdc644c85e2ae38e8b9f821ac3b4
Haruki Sakuta_作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-06-04 12:34:04 +0900

    Update CupertinoTimerPicker.dart

--- cupertino_widgets/CupertinoTimerPicker.dart
+++ cupertino_widgets/CupertinoTimerPicker.dart
@@ -1,3 +1,144 @@
 // Youtube URL: 
 // This is the code presented in the URL above.  
 // （上記URLで紹介されているコードです。）
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// How to use Apple's design countdown timer picker.
+/// Appleのデザインのカウントダウンタイマーのピッカーを使う方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('CupertinoTimerPicker')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  Duration duration = const Duration(hours: 1, minutes: 23);
+
+  // これは、CupertinoTimerPickerをホストする、
+  // 適度な固定高さのCupertinoModalPopupを示しています
+  void _showDialog(Widget child) {
+    showCupertinoModalPopup<void>(
+        context: context,
+        builder: (BuildContext context) => Container(
+              height: 216,
+              padding: const EdgeInsets.only(top: 6.0),
+              // Bottom marginは、
+              // ポップアップをシステムナビゲーションバーの上に配置するために用意されてる
+              margin: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              // ポップアップの背景色を指定
+              color: CupertinoColors.systemBackground.resolveFrom(context),
+              // SafeAreaウィジェットを使用して、システムのオーバーラップを回避する
+              child: SafeArea(
+                top: false,
+                child: child,
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: DefaultTextStyle(
+        style: TextStyle(
+          color: CupertinoColors.label.resolveFrom(context),
+          fontSize: 22.0,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _TimerPickerItem(
+                children: <Widget>[
+                  const Text('Timer'),
+                  CupertinoButton(
+                    // CupertinoTimerPickerを時・分モードで表示
+                    onPressed: () => _showDialog(
+                      /// Countdown timer picker settings
+                      /// カウントダウンタイマーピッカーの設定
+                      CupertinoTimerPicker(
+                        mode: CupertinoTimerPickerMode.hm,
+                        initialTimerDuration: duration,
+                        // ユーザーがタイマーの継続時間を変更したときに呼び出される
+                        onTimerDurationChanged: (Duration newDuration) {
+                          setState(() => duration = newDuration);
+                        },
+                      ),
+                    ),
+                    // この例では、タイマーの値は手動でフォーマットされています
+                    // intlパッケージを使用すると、
+                    // ユーザーのロケール設定に基づいて値をフォーマットすることができます
+                    child: Text(
+                      '$duration',
+                      style: const TextStyle(
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// このクラスは、単にウィジェットの列を装飾するものです。
+class _TimerPickerItem extends StatelessWidget {
+  const _TimerPickerItem({required this.children});
+
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: CupertinoColors.inactiveGray,
+            width: 0.0,
+          ),
+          bottom: BorderSide(
+            color: CupertinoColors.inactiveGray,
+            width: 0.0,
+          ),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: children,
+        ),
+      ),
+    );
+  }
+}
