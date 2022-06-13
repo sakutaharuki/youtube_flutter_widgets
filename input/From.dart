3408e0215ccfb0bad3a7a6ee0f9b960aade8a632
Haruki Sakuta_作田陽樹 <88698184+sakutaharuki@users.noreply.github.com>
2022-06-13 10:06:01 +0900

    Update From.dart

--- input/From.dart
+++ input/From.dart
@@ -1,1 +1,72 @@
+import 'package:flutter/material.dart';
+
+/// How to group inputs and buttons.
+/// 入力とボタンをグループ化する方法
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.yellow,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Form')),
+        body: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'メールアドレス',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'テキストを入力してください';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate：fromが有効ならtrue、無効ならfalse
+                if (_formKey.currentState!.validate()) {
+                  // Process data.
+                }
+              },
+              child: const Text('送信'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
